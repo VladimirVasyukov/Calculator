9f63e80ea9f8badb61b067b739cd955993f21103
Alexey Perov <external.java.courses@gmail.com>
2019-10-01 23:41:50 +0300

    Initial commit

---
+++ src/main/java/com/epam/java_basic/calculator/Calculator.java
@@ -0,0 +1,25 @@
+package com.epam.java_basic.calculator;
+
+public class Calculator {
+
+    public Calculator(int precision) {
+        throw new UnsupportedOperationException("You need to implement this method");
+    }
+
+    public double add(double a, double b) {
+        throw new UnsupportedOperationException("You need to implement this method");
+    }
+
+    public double subtract(double a, double b) {
+        throw new UnsupportedOperationException("You need to implement this method");
+    }
+
+    public double multiply(double a, double b) {
+        throw new UnsupportedOperationException("You need to implement this method");
+    }
+
+    public double div(double a, double b) {
+        throw new UnsupportedOperationException("You need to implement this method");
+    }
+
+}
